bc85c65b4308ad971126d89ceb1ef77f56e62b9e
ferreirasavio <saviio.ferreiira15@gmail.com>
2025-09-15 21:20:15 -0300

    adc log to DB

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -7,7 +7,13 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
+
+pool
+  .connect()
+  .then(() => console.log("✅ Connected to Supabase"))
+  .catch((err) => console.error("❌ Connection error:", err));
 
 export default pool;
